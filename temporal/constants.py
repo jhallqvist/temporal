d2db8cd0d3e4249ec490acb745a65fe3ce18b734
Jonas Hallqvist <lillhalkan@gmail.com>
2023-04-29 00:57:22 +0200

    Create constants.py

---
+++ temporal/constants.py
@@ -0,0 +1,23 @@
+DAY_NAMES           = ("Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun")
+DAY_NAMES_LONG      = ("Monday", "Tuesday", "Wednesday", "Thursday", "Friday",
+                        "Saturday", "Sunday")
+MONTH_NAMES         = ( "Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug",
+                         "Sep", "Oct", "Nov", "Dec")
+MONTH_NAMES_LONG    = ( "January", "Febuary", "March", "April", "May", "June",
+                        "July", "August", "September", "October", "November",
+                        "December")
+
+DAYS_IN_MONTH = {
+    1: 31,
+    2: 28,
+    3: 31,
+    4: 30,
+    5: 31,
+    6: 30,
+    7: 31,
+    8: 31,
+    9: 30,
+    10: 31,
+    11: 30,
+    12: 31
+}
